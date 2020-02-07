32b5404b0a7fa064e41b3af36e697c498ef82a78
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-02-07 18:47:46 +1100

    Initial setup

---
+++ src/templates.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function () {
+  return (
+    <div>
+    </div>
+  );
+}
+export default ;
+
+
+import React, { Component } from "react";
+
+class extends Component {
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+
+  //   };
+  // }
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+export default ;
