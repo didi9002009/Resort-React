4c7dda7b690a07578929ed7e4c79bfd6a6a3334a
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-02-11 13:44:39 +1100

    Update 1:50

---
+++ src/Context.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+const RoomContext = React.createContext();
+// <RoomContext.Provider value={}
+class RoomProvider extends Component {
+  state = {
+    greeting: "hello",
+    name: "Dan"
+  };
+  render() {
+    return (
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
+}
+
+const RoomConsumer = RoomContext.Consumer;
+
+export { RoomProvider, RoomConsumer, RoomContext };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 // import * as serviceWorker from './serviceWorker';
+import { RoomProvider } from "./Context";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   rootElement
 );
 
